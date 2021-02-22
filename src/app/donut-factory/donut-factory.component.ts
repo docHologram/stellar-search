237f3e851d6d81abcca1642d7aba8fbb6cacd8a0
Dave Hull <DHull@daytonfreight.com>
2021-02-21 21:20:44 -0500

    add icing

--- src/app/donut-factory/donut-factory.component.ts
+++ src/app/donut-factory/donut-factory.component.ts
@@ -1,10 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-import { interval } from 'rxjs';
-import { map, take, tap } from 'rxjs/operators';
+import { interval, Observable } from 'rxjs';
+import { concatMap, map, take, tap } from 'rxjs/operators';
+
+export enum Icing {
+	Vanilla = 'vanilla',
+	Chocolate = 'chocolate',
+	None = 'none'
+}
 
 export interface Donut {
-	currentPosition: number;
-	finalPosition: number;
+	icing: Icing;
+	sprinkles: boolean;
 }
 
 @Component({
@@ -13,22 +19,38 @@ export interface Donut {
 	styleUrls: ['./donut-factory.component.scss']
 })
 export class DonutFactoryComponent implements OnInit {
-	readonly leftPosition = 75;
-	readonly timeUnit = 1000;
-	readonly endingUnit = 900;
-	donuts: number[] = [];
+	readonly makerTimeUnit = 1000;
+	readonly icerTimeUnit = 1250;
+	donuts: Donut[] = [];
+	donutMaker$: Observable<number>;
+	donutIcer$: Observable<number>;
+
+	const newDonut: Donut = {
+		icing: Icing.None,
+		sprinkles: false
+	};
 
 	constructor() { }
 
 	ngOnInit(): void {
 
-		interval(this.timeUnit)
+		this.donutMaker$ = interval(this.makerTimeUnit)
 			.pipe(
+				tap((index: number) => this.donuts.push(this.newDonut)),
 				take(5)
+			);
+
+		this.donutIcer$ = interval(this.icerTimeUnit)
+			.pipe(
+				tap((index: number) => this.donuts[index].icing = Icing.Vanilla),
+				take(5)
+			);
+			
+		this.donutMaker$
+			.pipe(
+				concatMap(_ => this.donutIcer$)
 			)
-			.subscribe((index: number) => {
-				this.donuts.push(index);
-			});
+			.subscribe();
 	}
 
 
