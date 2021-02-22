440794a5c6b2667ad215af3c242890302c64d180
Dave Hull <DHull@daytonfreight.com>
2021-02-22 06:41:33 -0500

    working on concatting that map, foo

--- src/app/donut-factory/donut-factory.component.ts
+++ src/app/donut-factory/donut-factory.component.ts
@@ -1,16 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { interval, Observable } from 'rxjs';
-import { concatMap, map, take, tap } from 'rxjs/operators';
+import { concatMap, delay, map, mapTo, mergeMap, take, tap } from 'rxjs/operators';
 
-export enum Icing {
-	Vanilla = 'vanilla',
-	Chocolate = 'chocolate',
-	None = 'none'
+export enum Style {
+	Vanilla = 'donut-factory__donut--vanilla',
+	VanillaSprinkles = 'donut-factory__donut--vanilla-sprinkles',
+	Chocolate = 'donut-factory__donut--chocolate',
+	ChocolateSprinkles = 'donut-factory__donut--chocolate-sprinkles',
+	Plain = 'donut-factory__donut--plain'
 }
 
 export interface Donut {
-	icing: Icing;
-	sprinkles: boolean;
+	class: Style;
 }
 
 @Component({
@@ -19,36 +20,62 @@ export interface Donut {
 	styleUrls: ['./donut-factory.component.scss']
 })
 export class DonutFactoryComponent implements OnInit {
-	readonly makerTimeUnit = 1000;
-	readonly icerTimeUnit = 1250;
+	readonly timeUnit = 1000;
 	donuts: Donut[] = [];
+	freshBatch$: Observable<string>;
 	donutMaker$: Observable<number>;
 	donutIcer$: Observable<number>;
 
-	const newDonut: Donut = {
-		icing: Icing.None,
-		sprinkles: false
+	newDonut: Donut = {
+		class: Style.Plain
 	};
 
 	constructor() { }
 
 	ngOnInit(): void {
 
-		this.donutMaker$ = interval(this.makerTimeUnit)
+		this.freshBatch$ = interval(this.timeUnit)
 			.pipe(
-				tap((index: number) => this.donuts.push(this.newDonut)),
-				take(5)
+				tap((index: number) => {
+					this.donuts = [];
+					console.log(`fresh batch ${index}`)	
+				}),
+				mapTo('make another batch'),
+				take(1)
 			);
 
-		this.donutIcer$ = interval(this.icerTimeUnit)
+		this.donutMaker$ = interval(this.timeUnit)
 			.pipe(
-				tap((index: number) => this.donuts[index].icing = Icing.Vanilla),
-				take(5)
+				tap((index: number) => {
+					this.donuts.push(this.newDonut);
+					console.log(`make donut ${index + 1}`);
+				}),
+				take(5),
+				delay(4 * this.timeUnit)
+				/* delay(6 * this.timeUnit),
+				tap((index: number) => {
+					this.donuts[index].class = Style.Vanilla;
+					console.log(`style donut ${index + 1}`);	
+				}) */
+			);
+
+		this.donutIcer$ = interval(this.timeUnit)
+			.pipe(
+				tap((index: number) => {
+					if (this.donuts.length) {
+						this.donuts[index].class = Style.Vanilla;
+					}
+					
+					console.log(`style donut ${index + 1}`);	
+				}),
+				take(5),
+				delay(this.timeUnit)
 			);
 			
-		this.donutMaker$
+		this.donutIcer$
 			.pipe(
-				concatMap(_ => this.donutIcer$)
+				tap(_ => this.donuts = []),
+				concatMap(_ => this.donutMaker$)
 			)
 			.subscribe();
 	}
